17c915a21434bcfdbbd3e6299608c0bf0ca1852c
Vinny Williams <vinnywilliams13@gmail.com>
2020-09-29 20:57:56 -0400

    Add beginning of first encounter

--- AdventureGame.py
+++ AdventureGame.py
@@ -7,5 +7,30 @@ name = input()
 
 while name == "":
     print("Please enter your name.")
-    name = ""
-print("Welcome brave " + name + " to your first grand adventure!")
+    name = input()
+print("Welcome, brave " + name + ", to your first grand adventure!")
+
+time.sleep(1)
+
+print("Walking down the road to Northshire, you come across an giant snail attacking a merchant and his overturned wagon")
+time.sleep(1)
+print("What type of combat do you specialize in?")
+time.sleep(1)
+print("Melee")
+time.sleep(1)
+print("Ranged")
+time.sleep(1)
+print("Magic")
+weaponType = input()
+
+while weaponType.lower() not in ["melee", "ranged", "magic"]:
+        print("Please choose your combat specialization.")
+        weaponType = input()
+if weaponType.lower() == "melee":
+    print("You unsheathe your trusty sword and charge towards the beast!")
+elif weaponType.lower() == "ranged":
+    print("You nock an arrow and take aim at the beast!")
+elif weaponType.lower() == "magic":
+    print("You concentrate and feel lightning dance between your fingertips as you look towards the beast!")
+time.sleep(1)
+print("The snail moves slowly towards you")
